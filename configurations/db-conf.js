39bc393b1132b3db4aba2f71294bb951f19a7434
Humberto6641 <hgmezmartnez@gamil.com>
2021-09-18 22:41:57 -0600

    make it better

---
+++ configurations/db-conf.js
@@ -0,0 +1,9 @@
+const host= "localhost";
+const user= "root";
+const password= "Umg$2019";
+const database= "escuela";
+
+exports.host=host;
+exports.user=user;
+exports.password=password;
+exports.database=database;
